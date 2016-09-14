9c1a01d19ea32c6c1d66989d7869d71c0b54d014
Jessica Terry <jessica_terry@alumni.brown.edu>
2016-09-14 12:24:44 -0700

    setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './frontend/film_locations.jsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
+      }
+    ]
+  },
+  watch: true,
+  devtool: 'source-map'
+};
